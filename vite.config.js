d288bb4e3f06fc45daba530430f9d2fb6bed439f
Jonathan DiCecco <diceccoj@mcmaster.ca>
2023-01-10 11:06:26 -0500

    added billing system

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
         main: resolve(__dirname, 'index.html'),
         raspberry_pi_project: resolve(__dirname, 'raspberry_pi_project','index.html'),
         sudoku_solver: resolve(__dirname, 'sudoku_solver','index.html'),
+        billing_system: resolve(__dirname, 'billing_system','index.html'),
       },
     },
   },
