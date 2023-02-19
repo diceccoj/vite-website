ebaa61c410ad0abadfbc549507c164784a1044c3
Jonathan DiCecco <diceccoj@mcmaster.ca>
2023-02-19 13:35:42 -0500

    changed colour scheme

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
       input: {
         main: resolve(__dirname, 'index.html'),
         raspberry_pi_project: resolve(__dirname, 'raspberry_pi_project','index.html'),
-        sudoku_solver: resolve(__dirname, 'sudoku_solver','index.html'),
+        sudoku_solver: resolve(__dirname,'sudoku_solver','index.html'),
         billing_system: resolve(__dirname, 'billing_system','index.html'),
         cpu_fetcher: resolve(__dirname, 'cpu_fetcher','index.html'),
         tetris: resolve(__dirname, 'tetris','index.html'),
